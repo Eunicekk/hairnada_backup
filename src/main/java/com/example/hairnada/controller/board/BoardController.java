29c6bb8f3e8bcd966982fc3790a47a920f55f384
heeseok3886 <heesek3886@gmail.com>
2023-06-19 13:04:24 +0900

    작업1

--- src/main/java/com/example/hairnada/controller/board/BoardController.java
+++ src/main/java/com/example/hairnada/controller/board/BoardController.java
@@ -2,10 +2,20 @@ package com.example.hairnada.controller.board;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/board/*")
 @RequiredArgsConstructor
 public class BoardController {
+
+    @GetMapping("/communityList")
+    public void communityList(){}
+
+    @GetMapping("/communityRead")
+    public void communityRead(){}
+
+    @GetMapping("/communityWrite")
+    public void communityWrite(){}
 }
