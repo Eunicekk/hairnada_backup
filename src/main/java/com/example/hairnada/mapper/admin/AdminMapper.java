c08ef4a98f4fe74df2c2e9c01cce9866262faf95
leegaeuni <0923lge@naver.com>
2023-06-20 10:32:10 +0900

    ....

--- src/main/java/com/example/hairnada/mapper/admin/AdminMapper.java
+++ src/main/java/com/example/hairnada/mapper/admin/AdminMapper.java
@@ -1,7 +1,11 @@
 package com.example.hairnada.mapper.admin;
 
+import com.example.hairnada.dto.user.UserDto;
 import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
 
 @Mapper
 public interface AdminMapper {
+    public List<UserDto> selectUserList();
 }
