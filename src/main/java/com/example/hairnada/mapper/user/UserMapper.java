d84bff0a8c00fb0ab4a1a1dd84552611157addcb
asdfasdf <asdfasdf@asdfasdf>
2023-06-22 09:04:25 +0900

    userMapper commit

--- src/main/java/com/example/hairnada/mapper/user/UserMapper.java
+++ src/main/java/com/example/hairnada/mapper/user/UserMapper.java
@@ -1,7 +1,17 @@
 package com.example.hairnada.mapper.user;
 
+import com.example.hairnada.dto.user.UserDto;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 @Mapper
 public interface UserMapper {
+
+//    회원가입
+    public void join(UserDto userDto);
+
+//    로그인
+    public void selectUserNumber(@Param("userId") String userId, @Param("userPassword") String userPassword);
+
+//    계정찾기 smtp-> api 사용하기
 }
