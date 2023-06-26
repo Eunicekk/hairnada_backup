1e94ebf53ca837b74db9cbc61cc1edf7f23fd29d
asdfasdf <asdfasdf@asdfasdf>
2023-06-26 12:13:06 +0900

    myboard commit

---
+++ src/main/java/com/example/hairnada/mapper/user/MyLikeMapper.java
@@ -0,0 +1,14 @@
+package com.example.hairnada.mapper.user;
+
+import com.example.hairnada.vo.board.BoardVo;
+import com.example.hairnada.vo.page.Criteria03;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface MyLikeMapper {
+
+
+}
---
+++ src/test/java/com/example/hairnada/mapper/user/MyPageMapperTest.java
@@ -0,0 +1,14 @@
+package com.example.hairnada.mapper.user;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyPageMapperTest {
+
+    @Test
+    void myBoard() {
+
+    }
+
+}
